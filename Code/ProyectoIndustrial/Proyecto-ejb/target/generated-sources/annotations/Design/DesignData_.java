5b6d81fc87b474061a3bd08df954231cd3eb74dd
Daniel Gonzalez <danielgonzalezdagg@gmail.com>
2020-05-31 20:57:00 -0600

    Login cambiado

--- Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Design/DesignData_.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Design/DesignData_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-05-16T08:21:07")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-05-31T19:29:29")
 @StaticMetamodel(DesignData.class)
 public class DesignData_ { 
 
