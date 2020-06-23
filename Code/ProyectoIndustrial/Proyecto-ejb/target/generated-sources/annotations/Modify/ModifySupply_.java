9098717ff0431934d3783b8d23a26e63e96d05d6
Daniel Gonzalez <danielgonzalezdagg@gmail.com>
2020-06-22 19:45:50 -0600

    Tema finalizado

--- Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Modify/ModifySupply_.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Modify/ModifySupply_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-05-31T19:29:29")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-06-22T17:05:59")
 @StaticMetamodel(ModifySupply.class)
 public class ModifySupply_ { 
 
--- Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Supply/Measure_.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Supply/Measure_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-05-31T19:29:29")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-06-22T17:05:59")
 @StaticMetamodel(Measure.class)
 public class Measure_ { 
 
