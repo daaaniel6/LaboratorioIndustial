6c919372f33e6fdb94693838be44721813d6b9ae
angelRacancoj <orlan_1397rg@hotmail.com>
2020-08-06 21:04:01 -0600

    rest

---
+++ Code/ProyectoIndustrial/Proyecto-rest/src/test/java/gt/edu/usac/cunoc/ingenieria/config/RestApplication.java
@@ -0,0 +1,14 @@
+package gt.edu.usac.cunoc.ingenieria.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author angelrg
+ */
+@ApplicationPath("/api/v1")
+public class RestApplication extends Application{
+    
+}
