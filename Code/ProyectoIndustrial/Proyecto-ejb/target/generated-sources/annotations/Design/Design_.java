e0fdd023364efb2a3b6fe502bd0e5f26d0e19d2d
daaaniel6 <danielgonzalezdagg@gmail.com>
2020-10-29 21:18:57 -0600

    pom

--- Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Design/Design_.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/Design/Design_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-06-22T17:05:59")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-10-29T21:14:49")
 @StaticMetamodel(Design.class)
 public class Design_ { 
 
--- Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/User/Career_.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/target/generated-sources/annotations/User/Career_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-06-22T17:05:59")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2020-10-29T21:14:49")
 @StaticMetamodel(Career.class)
 public class Career_ { 
 
